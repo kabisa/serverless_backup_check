be009059d980489cbfcae6b2b58cb26721fef83d
Ralph Rooding <ralph@ralphrooding.com>
2019-11-11 11:20:56 +0100

    Update size_change_monitor.py

--- backup/size_change_monitor.py
+++ backup/size_change_monitor.py
@@ -2,6 +2,10 @@ import math
 
 
 def relative_size_change(current, previous):
+    '''
+    Return the percentage that the file size changed. The returned value is 
+    absolute (negative or positive change both returns a positive percentage).
+    '''
     return int(abs(((current - previous) / previous) * 100))
 
 
@@ -29,17 +33,13 @@ def allowed_size_change(bytesize):
     inc = 2.5
     dec = 2
 
-    # The minimum percentage to allow
-    minimum_percentage = 2
-
     order_of_magnitude = math.log10(bytesize)
 
-    allowed_percentage = (
+    minimum_percentage = 2
+    maximum_percentage = (
         math.exp(
             dec - math.sqrt(a * math.pow(order_of_magnitude, 2) + inc) / div
         ) * n
     )
 
-    # Return the largest of the 2 percentages. This makes sure that when
-    # allowed_percentages goes below minimum_percentage, minimum is returned
-    return int(max(minimum_percentage, allowed_percentage))
+    return int(max(minimum_percentage, maximum_percentage))
