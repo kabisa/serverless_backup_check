b2c3c69b3f13286f597493547c60d77babb2849f
Sjuul Janssen <sjuul@outlook.com>
2020-07-02 12:00:12 +0200

    fixed unit tests, also used Black

--- backup/server_stats.py
+++ backup/server_stats.py
@@ -2,39 +2,43 @@ import json
 from datetime import date, timedelta
 import humanize
 from backup.size_change_monitor import relative_size_change, allowed_size_change
-import os;
+import os
+
+assert os.sep == "/"
 
 
 def within_tolerance(current, previous):
-    'Checks if the backup size grew less than the allowed size.'
+    """Checks if the backup size grew less than the allowed size."""
     return relative_size_change(current, previous) <= allowed_size_change(previous)
 
 
 def date_to_prefix(prefix, date, file_date_format):
-    '''Converts a date to a S3 prefix of the form: prefix/year.month.day.
+    """
+    Converts a date to a S3 prefix of the form: prefix/year.month.day.
     Month and day are prepended with a leading zero if it is only 1 letter long.
-    '''
+    """
     if not file_date_format:
         file_date_format = "%Y.%m.%d"
-    
-    assert os.sep == '/'
+
     return os.path.join(prefix, date.strftime(file_date_format))
 
 
 def format_backup_size(backup_size):
-    '''Formats the size of a backup folder in a human readable format.
+    """
+    Formats the size of a backup folder in a human readable format.
 
     e.g. 1048576 => 1MB
 
     Returns the backup size as a human readable string
-    '''
+    """
     return humanize.naturalsize(backup_size, binary=True)
 
 
 def get_backup_prefix_keys(prefix, file_date_format):
-    '''Computes the prefixes that should only match a single backup folder in the S3 bucket.
+    """
+    Computes the prefixes that should only match a single backup folder in the S3 bucket.
     Returns tuple containing (backup_today_prefix, backup_yesterday_prefix).
-    '''
+    """
     today = date.today()
     one_day_ago = today - timedelta(1)
     two_days_ago = today - timedelta(2)
@@ -44,37 +48,42 @@ def get_backup_prefix_keys(prefix, file_date_format):
 
 
 class ServerStats(object):
-
-    def __init__(self, s3_client, folder, file_date_format):
+    def __init__(self, s3_client, folder, file_date_format=None):
         self.backup_folder = folder
         backup_folders = get_backup_prefix_keys(folder, file_date_format)
         self.last_size = s3_client.get_backup_size(backup_folders[0])
         self.second_last_size = s3_client.get_backup_size(backup_folders[1])
 
     @property
-    def status(self):
-        one_day_ago = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
-        two_days_ago = (date.today() - timedelta(2)).strftime('%Y-%m-%d')
-        if self.last_size == 0 and self.second_last_size == 0:
-            return f'Missing backup from {one_day_ago} and {two_days_ago}'
-        elif self.second_last_size == 0:
-            return f'Missing backup from {two_days_ago}'
-        elif self.last_size == 0:
-            return f'Missing backup from {one_day_ago}'
-        elif not within_tolerance(self.last_size, self.second_last_size):
-            return f'Backup size is outside tolerance'
+    def one_day_ago(self):
+        return (date.today() - timedelta(1)).strftime("%Y-%m-%d")
 
-        return 'Backup OK'
+    @property
+    def two_days_ago(self):
+        return (date.today() - timedelta(2)).strftime("%Y-%m-%d")
+
+    @property
+    def status(self):
+        if self.last_size == 0 and self.second_last_size == 0:
+            return f"Missing backup from {self.one_day_ago} and {self.two_days_ago}"
+        elif self.second_last_size == 0:
+            return f"Missing backup from {self.two_days_ago}"
+        elif self.last_size == 0:
+            return f"Missing backup from {self.one_day_ago}"
+        elif not within_tolerance(self.last_size, self.second_last_size):
+            return f"Backup size is outside tolerance"
+
+        return "Backup OK"
 
     @property
     def json(self):
-        'Returns status of the server as a JSON string.'
+        """Returns status of the server as a JSON string."""
         info = {
-            'backup_folder': self.backup_folder,
-            'backup_status': self.status,
-            'last_backup_size': format_backup_size(self.last_size),
-            'last_backup_date': (date.today() - timedelta(1)).strftime('%Y-%m-%d'),
-            'previous_backup_size': format_backup_size(self.second_last_size),
-            'previous_backup_date': (date.today() - timedelta(2)).strftime('%Y-%m-%d'),
+            "backup_folder": self.backup_folder,
+            "backup_status": self.status,
+            "last_backup_size": format_backup_size(self.last_size),
+            "last_backup_date": self.one_day_ago,
+            "previous_backup_size": format_backup_size(self.second_last_size),
+            "previous_backup_date": self.two_days_ago,
         }
-        return json.dumps(info)
+        return json.dumps(info, sort_keys=True)
